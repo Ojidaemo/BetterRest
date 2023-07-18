a3369807027e43921281e2872f339f2532a47183
Ojidaemo <vit.martsinovich@gmail.com>
2023-07-18 16:51:55 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vitali Martsinovich on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
